b5bb01e41c26f155cb010c86370019afab04e65c
amarseg <liamarseg@gmail.com>
2020-03-24 10:24:40 +0000

    Modify git ignore and add script to extract clusters from bed files

--- making_new_bed_files_with_interesting_genes.R
+++ making_new_bed_files_with_interesting_genes.R
@@ -1,4 +1,15 @@
 library(tidyverse)
 
-gene_list <- ''
+gene_list <- 'clusters_methyl.bed'
 genome_bed <- '../genome_bed.bed.txt'
+
+bed <- read_tsv(genome_bed, col_names = FALSE) 
+
+clusters <- read_tsv(gene_list, col_names = FALSE) %>%
+  filter(X7 == 'cluster 2')
+
+cl_genome_bed <- bed %>%
+  filter(X4 %in% clusclusters) 
+  
+write_tsv(bed,'clusters_bed.bed', col_names = FALSE)
+write_tsv(cl_genome_bed,'only_methyl.bed')
